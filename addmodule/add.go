a78f1a7022456beb4f98c630b8259a7c774c781e
Sachith Muhandiram <sachith@vizuamatix.com>
2019-09-14 22:15:38 +0530

    Container communication success

--- addmodule/add.go
+++ addmodule/add.go
@@ -5,15 +5,16 @@ import (
 	"net/http"
 )
 
-func add(res http.ResponseWriter, req *http.Request) {
-	log.Println("Request came to addmodule container")
-}
-
 func main() {
 	log.Println("Started addmodule")
+
 	http.HandleFunc("/add", add)
 	err := http.ListenAndServe("0.0.0.0:7070", nil)
 
 	log.Println("addmodule start values ", err)
 
+}
+
+func add(res http.ResponseWriter, req *http.Request) {
+	log.Println("Request came to addmodule container")
 }
--- mainmodule/main.go
+++ mainmodule/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,17 +8,9 @@ import (
 func main() {
 	log.Println("Starting main module")
 
-	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/add", add)
-	err := http.ListenAndServe(":7171", nil)
+	http.ListenAndServe(":7171", nil)
 
-	log.Println("Main module web server response : ", err)
-
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	log.Println("Test success")
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
 
 func add(res http.ResponseWriter, req *http.Request) {
